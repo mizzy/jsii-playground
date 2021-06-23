07f6aae2b9b5ab5c2a1cac071ad97598fa5125b7
Gosuke Miyashita <gosukenator@gmail.com>
2021-06-23 09:33:24 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+"use strict";
+var _a;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Greeter = void 0;
+const JSII_RTTI_SYMBOL_1 = Symbol.for("jsii.rtti");
+/**
+ * @experimental
+ */
+class Greeter {
+    /**
+     * @experimental
+     */
+    constructor(props) {
+        this.greetee = props.greetee;
+    }
+    /**
+     * @experimental
+     */
+    greet() {
+        return `Hello, ${this.greetee}!`;
+    }
+}
+exports.Greeter = Greeter;
+_a = JSII_RTTI_SYMBOL_1;
+Greeter[_a] = { fqn: "jsii-playground.Greeter", version: "1.0.0" };
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUlBLE1BQWEsT0FBTzs7OztJQUdoQixZQUFtQixLQUFtQjtRQUNsQyxJQUFJLENBQUMsT0FBTyxHQUFHLEtBQUssQ0FBQyxPQUFPLENBQUM7SUFDakMsQ0FBQzs7OztJQUVNLEtBQUs7UUFDUixPQUFPLFVBQVUsSUFBSSxDQUFDLE9BQU8sR0FBRyxDQUFBO0lBQ3BDLENBQUM7O0FBVEwsMEJBVUMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgaW50ZXJmYWNlIEdyZWV0ZXJQcm9wcyB7XG4gICAgcmVhZG9ubHkgZ3JlZXRlZTogc3RyaW5nO1xufVxuXG5leHBvcnQgY2xhc3MgR3JlZXRlciB7XG4gICAgcHJpdmF0ZSByZWFkb25seSBncmVldGVlOiBzdHJpbmc7XG5cbiAgICBwdWJsaWMgY29uc3RydWN0b3IocHJvcHM6IEdyZWV0ZXJQcm9wcykge1xuICAgICAgICB0aGlzLmdyZWV0ZWUgPSBwcm9wcy5ncmVldGVlO1xuICAgIH1cblxuICAgIHB1YmxpYyBncmVldCgpOiBzdHJpbmcge1xuICAgICAgICByZXR1cm4gYEhlbGxvLCAke3RoaXMuZ3JlZXRlZX0hYFxuICAgIH1cbn1cbiJdfQ==
